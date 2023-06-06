fc3cdc71ae5e565539886d36a72f8de21062b1fe
anton <96013226+ANTONCHTHON@users.noreply.github.com>
2023-06-06 09:49:57 +0300

    Update and rename main.py to cars_bot/main.py

--- cars_bot/main.py
+++ cars_bot/main.py
@@ -3,13 +3,13 @@ import logging
 import asyncio
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import config
+import cars_bot.config as config
 
 loop = asyncio.new_event_loop()
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot, loop=loop, storage=MemoryStorage())
 
-if __name__ == '__main__':
-    from handlers import dp
+def main():
+    from cars_bot.handlers import dp
 
     executor.start_polling(dp)
